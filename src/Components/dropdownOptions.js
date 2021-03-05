442970881b3479cf2581891fb4bd7d38f761b3d8
Animesh25 <animeshshankar25@gmail.com>
2021-03-05 17:56:07 +0000

    small boxed maze created

--- src/Components/dropdownOptions.js
+++ src/Components/dropdownOptions.js
@@ -5,5 +5,5 @@ export const directionOptions = [
     '4-Directional', '8-Directional'
 ]
 export const mazeOptions = [
-    'Loop', 'Maze 1', 'Maze 2', 'Boxed'
+    'Loop', 'Maze 1', 'Maze 2', 'Boxed','Small Boxed'
 ]
